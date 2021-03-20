88ba937b9ba72b057ec34afafe0f6469094175f4
bsmrvl <ben.j.somerville@icloud.com>
2021-03-20 11:10:42 -0400

    query.php

---
+++ query.php
@@ -0,0 +1,27 @@
+<?php
+
+$conn = new mysqli(
+    "127.0.0.1",
+    getenv('MYSQL_USER'),
+    getenv('MYSQL_PW'),
+    getenv('MYSQL_DB')
+);
+
+if ($conn -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+}
+
+$qry = $_POST['q'];
+
+if ($result = $conn -> query($qry)) {
+    while ($row = $result -> fetch_assoc()) {
+        $all[] = $row;
+    }
+    echo json_encode($all);
+    $result -> free_result();
+}
+  
+$conn -> close();
+
+?>
